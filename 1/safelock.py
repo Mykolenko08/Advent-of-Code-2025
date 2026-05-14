b4b730f2617e8e702ca871905b1eb5a0a65385c4
Mykolenko08 <v.mykolenko_fit_16_25_b_d@knute.edu.ua>
2026-05-14 19:20:15 +0300

    1

---
+++ 1/safelock.py
@@ -0,0 +1,19 @@
+﻿a = 50
+zeros = 0
+
+while True:
+	way = input("Введіть L чи R: ")
+	if way == "L":
+		m = int(input("Введіть значення (L): "))
+		a = a - m
+		if a < 0:
+			a = a + 100
+	else:
+		m = int(input("Введіть значення (R): "))
+		a = a + m
+		if a > 99:
+			a = a - 100
+	print(a)
+	if a == 0:
+		zeros = zeros + 1
+	print(f"Нулів: {zeros}")
