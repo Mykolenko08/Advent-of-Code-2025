fdd44e9337ae4b50820a98984f618243bed882c2
Mykolenko08 <v.mykolenko_fit_16_25_b_d@knute.edu.ua>
2026-05-14 20:57:09 +0300

    Add files via upload

---
+++ 1/part2.py
@@ -0,0 +1,19 @@
+﻿a = 50
+zeros = 0
+
+while True:
+    move = input("Ln/Rn: ").strip()
+
+    m = int(move[1:])
+    old = a
+
+    if move[0] == "L":
+        zeros = zeros + (m - old + 99) // 100
+        a = (a - m) % 100
+
+    else:
+        zeros = zeros + (old + m) // 100
+        a = (a + m) % 100
+
+    print(a)
+    print(f"Нулів: {zeros}")
